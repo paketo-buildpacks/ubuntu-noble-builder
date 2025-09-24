4cdaafbfbecb3901bc19c81db2d59af575bd70bc
Costas Papastathis <papastathiscr@gmail.com>
2025-09-24 22:48:44 +0200

    Changing pull policy to always on integration tests

--- smoke/procfile_test.go
+++ smoke/procfile_test.go
@@ -56,7 +56,7 @@ func testProcfile(t *testing.T, context spec.G, it spec.S) {
 			var err error
 			var logs fmt.Stringer
 			image, logs, err = pack.Build.
-				WithPullPolicy("never").
+				WithPullPolicy("awlays").
 				WithBuilder(Builder).
 				WithBuildpacks(
 					config.Procfile,
