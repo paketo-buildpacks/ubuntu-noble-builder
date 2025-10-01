08f6ec505b1683a2f928813142634cb4ecb49ed3
Costas Papastathis <papastathiscr@gmail.com>
2025-10-01 20:11:17 +0300

    converting builder to support arm64 and adding leaving only nodejs buildpack

--- builders/builder/smoke/init_test.go
+++ builders/builder/smoke/init_test.go
@@ -29,14 +29,6 @@ func TestSmoke(t *testing.T) {
 	SetDefaultEventuallyTimeout(60 * time.Second)
 
 	suite := spec.New("Smoke", spec.Parallel(), spec.Report(report.Terminal{}))
-	suite("Dotnet", testDotnet)
-	suite("Go", testGo)
-	suite("Java Native Image", testJavaNativeImage)
-	suite("Java", testJava)
 	suite("Node.js", testNodejs)
-	suite("Procfile", testProcfile)
-	suite("Python", testPython)
-	suite("Ruby", testRuby)
-	suite("Web Servers", testWebServers)
 	suite.Run(t)
 }
