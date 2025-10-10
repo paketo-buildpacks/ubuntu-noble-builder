827f54ddd0bd6335c9ded1e3eaad749c254de1cc
Costas Papastathis <papastathiscr@gmail.com>
2025-10-10 14:50:37 +0200

    Update builders/builder/smoke/java_test.go

--- builders/builder/smoke/java_test.go
+++ builders/builder/smoke/java_test.go
@@ -52,7 +52,7 @@ func testJava(t *testing.T, context spec.G, it spec.S) {
 		it("builds successfully", func() {
 			var err error
 			// this is OK, the app also works for running with a JVM
-			source, err = occam.Source(filepath.Join("testdata", "java-native-image"))
+			source, err = occam.Source(filepath.Join("testdata", "spring-boot-native-image-maven"))
 			Expect(err).NotTo(HaveOccurred())
 
 			var logs fmt.Stringer
